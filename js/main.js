fd8e459053f878a6fe2b3f2c494158b7bef0f7d3
pohern <ohern_p@yahoo.com>
2020-11-14 14:26:18 -0600

    working on winning logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,55 @@
+console.log('sanity check')
+/*----- constants -----*/
+const winningCombinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+/*----- app's state (variables) -----*/
+let board;
+let turn = 'X';
+let win;
+/*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'))
+const messages = document.querySelector('h2')
+/*----- event listeners -----*/
+document.getElementById('board').addEventListener('click',handleTurn);
+/*----- functions -----*/
+function init() {
+    board = [
+        '','','',
+        '','','',
+        '','',''
+    ];
+};
+init()
+
+function render() {
+    board.forEach(function(mark,index){
+        squares[index].textContent = mark;
+    });
+    messages.textContent = `It's ${turn}'s turn!`
+};
 
 
+function handleTurn(event){
+    let idx = squares.findIndex(function(square){
+        return square === event.target;
+    });
+    board[idx] = turn;
+    turn = turn === 'X' ? 'O' : 'X';
+    win = getWinner();
+    render()
+};
 
+function getWinner(){
 
-
-
+};
 
 
 

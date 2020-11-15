c3d8f85495dc3247113f5e96b64fba1d8007930c
pohern <ohern_p@yahoo.com>
2020-11-15 10:53:29 -0600

    almost done

--- js/main.js
+++ js/main.js
@@ -1,55 +1,136 @@
-console.log('sanity check')
 /*----- constants -----*/
-const winningCombinations = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-];
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
+
 /*----- app's state (variables) -----*/
+
 let board;
 let turn = 'X';
 let win;
+
 /*----- cached element references -----*/
-const squares = Array.from(document.querySelectorAll('#board div'))
-const messages = document.querySelector('h2')
+
+const squares = Array.from(document.querySelectorAll('#board div'));
+
 /*----- event listeners -----*/
-document.getElementById('board').addEventListener('click',handleTurn);
+document.getElementById('board').addEventListener('click', handleTurn);
+const messages = document.querySelector('h2');
+document.getElementById('reset-button').addEventListener('click', init);
+
+
 /*----- functions -----*/
-function init() {
-    board = [
-        '','','',
-        '','','',
-        '','',''
-    ];
-};
-init()
 
-function render() {
-    board.forEach(function(mark,index){
-        squares[index].textContent = mark;
-    });
-    messages.textContent = `It's ${turn}'s turn!`
+function getWinner() {
+    let winner = null;
+    winningCombos.forEach(function(combo, index) {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+        });
+        return winner ? winner : board.includes('') ? null : 'T';
 };
 
-
-function handleTurn(event){
-    let idx = squares.findIndex(function(square){
+function handleTurn() {
+    let idx = squares.findIndex(function(square) {
         return square === event.target;
     });
     board[idx] = turn;
     turn = turn === 'X' ? 'O' : 'X';
     win = getWinner();
-    render()
+    render();
 };
 
-function getWinner(){
-
+function init() {
+    board = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+    ];
+    render();
 };
+
+function render() {
+    board.forEach(function(mark, index) {
+    //this moves the value of the board item into the squares[idx]
+    squares[index].textContent = mark;
+    });
+    messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+    };
+
+init();
+
+
+
+
+
+
+
+// console.log('sanity check')
+// /*----- constants -----*/
+// const winningCombinations = [
+//     [0,1,2],
+//     [3,4,5],
+//     [6,7,8],
+//     [0,3,6],
+//     [1,4,7],
+//     [2,5,8],
+//     [0,4,8],
+//     [2,4,6]
+// ];
+// /*----- app's state (variables) -----*/
+// let board;
+// let turn = 'X';
+// let win;
+// /*----- cached element references -----*/
+// const squares = Array.from(document.querySelectorAll('#board div'))
+// const messages = document.querySelector('h2')
+// /*----- event listeners -----*/
+// document.getElementById('board').addEventListener('click',handleTurn);
+// document.getElementById('reset-button').addEventListener('click', init);
+// /*----- functions -----*/
+
+
+// function init() {
+//     board = [
+//         '','','',
+//         '','','',
+//         '','',''
+//     ];
+//     render();
+// };
+// init()
+
+// function render() {
+//     board.forEach(function(mark,index){
+//         squares[index].textContent = mark;
+//     });
+//     messages.textContent = win === 'T' ? `That's a tie!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+// };
+
+
+// function handleTurn(event){
+//     let idx = squares.findIndex(function(square){
+//         return square === event.target;
+//     });
+//     board[idx] = turn;
+//     turn = turn === 'X' ? 'O' : 'X';
+//     win = getWinner();
+//     render();
+// };
+
+// function getWinner(){
+//     let winner = null;
+//     winningCombinations.forEach(function(combo,index){
+//         if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+//     });
+//     return winner ? winner : board.includes('') ? null : 'T';
+// };
 
 
 
